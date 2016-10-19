4c65d306b76872fcb50a8604776589c7a1cf5e47
Jimut Dhali <jemdhali67@gmail.com>
2016-10-19 22:47:53 +0530

    Added initial migrations

---
+++ database/migrations/2016_10_19_164247_create_donors_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDonorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('donors', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->string('avatar')->nullable();
+            $table->string('name');
+            $table->string('dob');
+            $table->text('address');
+            $table->string('contact_no');
+            $table->string('blood_type');
+            $table->text('health_issues')->nullable();
+
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')
+                  ->references('id')->on('users')
+                  ->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('donors');
+    }
+}
---
+++ database/migrations/2016_10_19_164342_create_hospitals_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateHospitalsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('hospitals', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->string('avatar')->nullable();
+            $table->string('name');
+            $table->string('reg_no');
+            $table->string('address');
+            $table->string('contact_no');
+
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')
+                  ->references('id')->on('users')
+                  ->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('hospitals');
+    }
+}
