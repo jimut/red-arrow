dc698b50a780aadfb26da88a7921eeaeb8251702
Jimut Dhali <jemdhali67@gmail.com>
2016-10-21 22:15:03 +0530

    Changed hospital display style

--- resources/views/hospital/show.blade.php
+++ resources/views/hospital/show.blade.php
@@ -7,9 +7,9 @@
         <div class="col-md-8 col-md-offset-4">
             <h1>{{ $hospital->name }}</h1>
             <p>
-                Regitration Number: {{ $hospital->reg_no }} <br>
-                Contact Number: {{ $hospital->contact_no }} <br>
-                Address: {{ $hospital->address }} 
+                <strong>Regitration Number:</strong> {{ $hospital->reg_no }} <br>
+                <strong>Contact Number:</strong> {{ $hospital->contact_no }} <br>
+                <strong>Address:</strong> {{ $hospital->address }} 
             </p>
         </div>
     </div>
