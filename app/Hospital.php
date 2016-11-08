460af6ec9f164653f1a260c09717867b5831c3ce
Jimut Dhali <jemdhali67@gmail.com>
2016-11-08 15:13:51 +0530

    Added relationship functions

--- app/Hospital.php
+++ app/Hospital.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Hospital extends Model
 {
-    //
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
 }
--- app/User.php
+++ app/User.php
@@ -26,4 +26,14 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function donor()
+    {
+        return $this->hasOne('App\Donor');
+    }
+
+    public function hospital()
+    {
+        return $this->hasOne('App\Hospital');
+    }
 }
