e4c7d57839df6580c6bbf8891b660775891d5b73
Jimut Dhali <jemdhali67@gmail.com>
2016-12-08 00:11:24 +0530

    Installed Passport

--- config/app.php
+++ config/app.php
@@ -166,8 +166,7 @@ return [
         /*
          * Package Service Providers...
          */
-
-        //
+        Laravel\Passport\PassportServiceProvider::class,
 
         /*
          * Application Service Providers...
