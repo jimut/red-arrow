42c7d70b525b75b6acf205dc742517b827e96853
Jimut Dhali <jemdhali67@gmail.com>
2017-02-21 22:15:02 +0530

    Frontend for auth pages

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,96 +1,83 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Login</div>
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+<div class="flex-screen-center">
+    <div class="card-sm">
+        <h1>Login</h1>
 
-                    @if (session('warning'))
-                        <div class="alert alert-warning">
-                            {{ session('warning') }}
-                        </div>
-                    @endif
+        <div class="divider-10"></div>
 
-                    <form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
-                        {{ csrf_field() }}
+        @if (session('status'))
+            <div class="alert alert-success">
+                {{ session('status') }}
+            </div>
+        @endif
 
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+        @if (session('warning'))
+            <div class="alert alert-warning">
+                {{ session('warning') }}
+            </div>
+        @endif
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
+        <form role="form" method="POST" action="{{ url('/login') }}">
+            {{ csrf_field() }}
 
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
+            <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                <label for="email" class="control-label">E-Mail Address</label>
 
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
+                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
 
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
+                @if ($errors->has('email'))
+                    <span class="help-block">
+                        <strong>{{ $errors->first('email') }}</strong>
+                    </span>
+                @endif
+            </div>
 
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
+            <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                <label for="password" class="control-label">Password</label>
 
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" name="remember"> Remember Me
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
+                <input id="password" type="password" class="form-control" name="password" required>
 
-                        <div class="form-group">
-                            <div class="col-md-8 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary" disabled>
-                                    Login
-                                </button>
+                @if ($errors->has('password'))
+                    <span class="help-block">
+                        <strong>{{ $errors->first('password') }}</strong>
+                    </span>
+                @endif
+            </div>
 
-                                <a class="btn btn-link" href="{{ url('/password/reset') }}">
-                                    Forgot Your Password?
-                                </a>
-                            </div>
-                        </div>
-                    </form>
-
-                    <hr>
-
-                    <div class="row">
-                        <div class="col-md-6 col-md-offset-4">
-                            <a href="{{ url('auth/facebook') }}" id="facebook-login-link" class="btn btn-primary btn-facebook" disabled>
-                                <i class="fa fa-facebook-official"></i>Continue With Facebook
-                            </a>
-
-                            <div style="margin-bottom: 15px;"></div>
-
-                            <a href="{{ url('auth/google') }}" id="google-login-link" class="btn btn-primary btn-google" disabled>
-                                <i class="fa fa-google"></i>Continue With Google
-                            </a>
-                        </div>
-                    </div>
+            <div class="form-group">
+                <div class="checkbox">
+                    <label>
+                        <input type="checkbox" name="remember"> Remember Me
+                    </label>
                 </div>
             </div>
-        </div>
+
+            <div class="form-group">
+                <button type="submit" class="btn btn-primary" disabled>
+                    Login
+                </button>
+
+                <a class="btn btn-link" href="{{ url('/password/reset') }}">
+                    Forgot Your Password?
+                </a>
+            </div>
+        </form>
+
+        <hr>
+
+        <a href="{{ url('auth/facebook') }}" id="facebook-login-link" class="btn btn-primary btn-facebook" disabled>
+            <i class="fa fa-facebook-official"></i>Continue With Facebook
+        </a>
+
+        <div class="divider-15"></div>
+
+        <a href="{{ url('auth/google') }}" id="google-login-link" class="btn btn-primary btn-google" disabled>
+            <i class="fa fa-google"></i>Continue With Google
+        </a>
     </div>
 </div>
+
+<div class="backdrop"></div>
 @endsection
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,98 +1,83 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Register</div>
-                <div class="panel-body">
-                    <form class="form-horizontal" role="form" method="POST" action="{{ url('/register') }}">
-                        {{ csrf_field() }}
+<div class="flex-screen-center">
+    <div class="card-sm">
+        <h1>Register</h1>
 
-                        <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-                            <label for="name" class="col-md-4 control-label">Name</label>
+        <div class="divider-10"></div>
 
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
+        <form role="form" method="POST" action="{{ url('/register') }}">
+            {{ csrf_field() }}
 
-                                @if ($errors->has('name'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('name') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
+            <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
+                <label for="name" class="control-label">Name</label>
 
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+                <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('password_confirmation') ? ' has-error' : '' }}">
-                            <label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
-
-                                @if ($errors->has('password_confirmation'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password_confirmation') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Register
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-
-                    <hr>
-
-                    <div class="row">
-                        <div class="col-md-6 col-md-offset-4">
-                            <a href="{{ url('auth/facebook') }}" id="facebook-login-link" class="btn btn-primary btn-facebook" disabled>
-                                <i class="fa fa-facebook-official"></i>Continue With Facebook
-                            </a>
-
-                            <div style="margin-bottom: 15px;"></div>
-
-                            <a href="{{ url('auth/google') }}" id="google-login-link" class="btn btn-primary btn-google" disabled>
-                                <i class="fa fa-google"></i>Continue With Google
-                            </a>
-                        </div>
-                    </div>
-                </div>
+                @if ($errors->has('name'))
+                    <span class="help-block">
+                        <strong>{{ $errors->first('name') }}</strong>
+                    </span>
+                @endif
             </div>
-        </div>
+
+            <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                <label for="email" class="control-label">E-Mail Address</label>
+
+                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
+
+                @if ($errors->has('email'))
+                    <span class="help-block">
+                        <strong>{{ $errors->first('email') }}</strong>
+                    </span>
+                @endif
+            </div>
+
+            <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                <label for="password" class="control-label">Password</label>
+
+                <input id="password" type="password" class="form-control" name="password" required>
+
+                @if ($errors->has('password'))
+                    <span class="help-block">
+                        <strong>{{ $errors->first('password') }}</strong>
+                    </span>
+                @endif
+            </div>
+
+            <div class="form-group{{ $errors->has('password_confirmation') ? ' has-error' : '' }}">
+                <label for="password-confirm" class="control-label">Confirm Password</label>
+
+                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
+
+                @if ($errors->has('password_confirmation'))
+                    <span class="help-block">
+                        <strong>{{ $errors->first('password_confirmation') }}</strong>
+                    </span>
+                @endif
+            </div>
+
+            <div class="form-group">
+                <button type="submit" class="btn btn-primary">
+                    Register
+                </button>
+            </div>
+        </form>
+
+        <hr>
+
+        <a href="{{ url('auth/facebook') }}" id="facebook-login-link" class="btn btn-primary btn-facebook" disabled>
+            <i class="fa fa-facebook-official"></i>Continue With Facebook
+        </a>
+
+        <div class="divider-15"></div>
+
+        <a href="{{ url('auth/google') }}" id="google-login-link" class="btn btn-primary btn-google" disabled>
+            <i class="fa fa-google"></i>Continue With Google
+        </a>
     </div>
 </div>
+
+<div class="backdrop"></div>
 @endsection
